bb43bba776717608988083d5f27b0b549486cfa5
achied <achied@gmail.com>
2013-02-27 01:45:11 +0100

    refactoring test code

--- test/plugin/in_nats.rb
+++ test/plugin/in_nats.rb
@@ -12,32 +12,66 @@ class NATSInputTest < Test::Unit::TestCase
     host localhost
     user nats
     password nats
-    queue fluent.>,fluent2.>
   ]
 
-  
+  def basic_queue_conf
+    conf = CONFIG + %[
+      queue fluent.>
+    ]
+  end
+
+  def multiple_queue_conf
+    conf = CONFIG + %[
+      queue fluent.>, fluent2.>
+    ]
+  end
+
+  def ssl_conf
+    conf = basic_queue_conf + %[
+      ssl true
+    ]
+  end
+
   def create_driver(conf=CONFIG)
     Fluent::Test::InputTestDriver.new(Fluent::NATSInput).configure(conf)
   end
   
-  def test_configure
-    d = create_driver
+  def test_configure_basic
+    d = create_driver basic_queue_conf
     assert_equal 4222, d.instance.port
     assert_equal 'localhost', d.instance.host
     assert_equal 'nats', d.instance.user
     assert_equal 'nats', d.instance.password
-    assert_equal 'fluent.>,fluent2.>', d.instance.queue
+    assert_equal 'fluent.>', d.instance.queue
+  end
+
+  def test_configure_multiple_queue
+    d = create_driver multiple_queue_conf
+    assert_equal 4222, d.instance.port
+    assert_equal 'localhost', d.instance.host
+    assert_equal 'nats', d.instance.user
+    assert_equal 'nats', d.instance.password
+    assert_equal 'fluent.>, fluent2.>', d.instance.queue
+  end
+
+  def test_configure_basic_with_ssl
+    d = create_driver ssl_conf
+    assert_equal 4222, d.instance.port
+    assert_equal 'localhost', d.instance.host
+    assert_equal 'nats', d.instance.user
+    assert_equal 'nats', d.instance.password
+    assert_equal 'fluent.>', d.instance.queue
+    assert_equal true, d.instance.ssl
   end
 
   def test_emit_with_credentials
-    d = create_driver
+    d = create_driver basic_queue_conf
 
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     Fluent::Engine.now = time
 
     d.expect_emit "nats.fluent.test1", time, {"message"=>'nats', "fluent_timestamp"=>time}
     d.expect_emit "nats.fluent.test2", time, {"message"=>'nats', "fluent_timestamp"=>time}
-    d.expect_emit "nats.fluent2.test1", time, {"message"=>'nats', "fluent_timestamp"=>time}
 
     uri = "nats://#{d.instance.user}:#{d.instance.password}@#{d.instance.host}:#{d.instance.port}"
 
@@ -52,14 +86,13 @@ class NATSInputTest < Test::Unit::TestCase
   end
 
   def test_emit_without_credentials
-    d = create_driver
+    d = create_driver basic_queue_conf
 
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     Fluent::Engine.now = time
 
     d.expect_emit "nats.fluent.test1", time, {"message"=>'nats', "fluent_timestamp"=>time}
     d.expect_emit "nats.fluent.test2", time, {"message"=>'nats', "fluent_timestamp"=>time}
-    d.expect_emit "nats.fluent2.test1", time, {"message"=>'nats', "fluent_timestamp"=>time}
 
     uri = "nats://#{d.instance.host}:#{d.instance.port}"
 
@@ -73,14 +106,37 @@ class NATSInputTest < Test::Unit::TestCase
     kill_nats
   end
 
+  def test_emit_multiple_queues
+    d = create_driver multiple_queue_conf
+
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    Fluent::Engine.now = time
+
+    d.expect_emit "nats.fluent.test1", time, {"message"=>'nats', "fluent_timestamp"=>time}
+    d.expect_emit "nats.fluent.test2", time, {"message"=>'nats', "fluent_timestamp"=>time}
+    d.expect_emit "nats.fluent2.test1", time, {"message"=>'nats', "fluent_timestamp"=>time}
+    d.expect_emit "nats.fluent2.test2", time, {"message"=>'nats', "fluent_timestamp"=>time}
+
+    uri = "nats://#{d.instance.host}:#{d.instance.port}"
+
+    start_nats(uri)
+    d.run do
+      d.expected_emits.each { |tag, time, record|
+        send(uri, tag[5..-1], record)
+        sleep 0.5
+      }
+    end
+    kill_nats
+  end
+
+
   def test_emit_without_fluent_timestamp
-    d = create_driver
+    d = create_driver basic_queue_conf
 
     time = Time.now.to_i
     Fluent::Engine.now = time
 
     d.expect_emit "nats.fluent.test1", time, {"message"=>'nats'}
-    d.expect_emit "nats.fluent2.test1", time, {"message"=>'nats'}
 
     uri = "nats://#{d.instance.host}:#{d.instance.port}"
     start_nats(uri)
@@ -94,15 +150,13 @@ class NATSInputTest < Test::Unit::TestCase
   end
 
   def test_emit_arrays
-    d = create_driver
+    d = create_driver basic_queue_conf
 
     time = Time.now.to_i
     Fluent::Engine.now = time
   
     d.expect_emit "nats.fluent.empty_array", time, []
     d.expect_emit "nats.fluent.string_array", time, %w(one two three)
-    d.expect_emit "nats.fluent2.empty_array", time, []
-    d.expect_emit "nats.fluent2.string_array", time, %w(one two three)
 
     uri = "nats://#{d.instance.host}:#{d.instance.port}"
     start_nats(uri)
@@ -116,23 +170,18 @@ class NATSInputTest < Test::Unit::TestCase
   end
 
   def test_empty_publish_string
-    d = create_driver
+    d = create_driver basic_queue_conf
 
     time = Time.now.to_i
     Fluent::Engine.now = time
 
     d.expect_emit "nats.fluent.nil", time, nil
-    d.expect_emit "nats.fluent2.nil", time, nil
 
     uri = "nats://#{d.instance.host}:#{d.instance.port}"
     start_nats(uri)
     d.run do
       d.expected_emits.each do |tag, time, record|
-        EM.run do
-          n = NATS.connect uri: uri
-          n.publish tag[5..-1]
-          n.close
-        end
+        send(uri, tag[5..-1], nil)
         sleep 0.5
       end
     end
@@ -140,29 +189,23 @@ class NATSInputTest < Test::Unit::TestCase
   end
 
   def test_regular_publish_string
-    d = create_driver
+    d = create_driver basic_queue_conf
 
     time = Time.now.to_i
     Fluent::Engine.now = time
 
     d.expect_emit "nats.fluent.string", time, "Lorem ipsum dolor sit amet"
-    d.expect_emit "nats.fluent2.string", time, "Lorem ipsum dolor sit amet"
 
     uri = "nats://#{d.instance.host}:#{d.instance.port}"
     start_nats(uri)
     d.run do
       d.expected_emits.each do |tag, time, record|
-        EM.run do
-          n = NATS.connect uri: uri
-          n.publish tag[5..-1], "Lorem ipsum dolor sit amet"
-          n.close
-        end
+        send(uri, tag[5..-1], "Lorem ipsum dolor sit amet")
         sleep 0.5
       end
     end
     kill_nats
   end
-
 
   def setup
     Fluent::Test.setup
